aaf72d3f6423dcf5a44736e6b4fff05c9619faaa
sabaja <jacoposabatini76@gmail.com>
2019-02-03 22:42:54 +0100

    Bug fixing

--- src/main/java/microservices/book/gateway/GatewayApplication.java
+++ src/main/java/microservices/book/gateway/GatewayApplication.java
@@ -23,14 +23,18 @@ public class GatewayApplication {
 	public static void main(String[] args) throws IOException {
 		log.info("Preparing to run application");
 		ConfigurableApplicationContext ctx = SpringApplication.run(GatewayApplication.class, args);
-		log.info("Contex is running @{}\nHit enter to stop server",
+		log.info("\n{}\nContext is running...\nStarted @[{}] \nHit enter to stop server",
+				ctx.getDisplayName(),
 				Instant.ofEpochMilli(ctx.getStartupDate()).atZone(ZoneId.systemDefault()).toLocalDateTime());
 		System.in.read();
 		ctx.close();
 	}
 
-	@Bean
-	public RequestFilter requestFilter() {
-		return new RequestFilter();
-	}
+//	@Bean//	@Bean
+//	public RequestFilter requestFilter() {
+//	return new RequestFilter();
+//}
+//	public RequestFilter requestFilter() {
+//		return new RequestFilter();
+//	}
 }
