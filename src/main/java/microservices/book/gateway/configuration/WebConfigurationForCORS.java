7843e518d8100cfc29eec32e9b9b53a3cfefc098
sabaja <jacoposabatini76@gmail.com>
2019-02-03 18:28:31 +0100

    Add WebConfigurationForCORS

---
+++ src/main/java/microservices/book/gateway/configuration/WebConfigurationForCORS.java
@@ -0,0 +1,31 @@
+package microservices.book.gateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+
+/**
+ * https://www.logicbig.com/how-to/code-snippets/jcode-spring-mvc-webmvcconfigurer.html
+ * 
+ * Enables Cross-Origin Resource Sharing (CORS) More info:
+ * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
+ * 
+ * @param registry
+ * 
+ * @author sabaja
+ *
+ */
+public class WebConfigurationForCORS implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**");
+	}
+
+}
