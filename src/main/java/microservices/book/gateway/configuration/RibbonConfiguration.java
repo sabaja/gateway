4f8210042a213e23635b6833658abf5be90f4eef
sabaja <jacoposabatini76@gmail.com>
2019-02-23 22:01:31 +0100

    Add RibboncConfig

--- src/main/java/microservices/book/gateway/configuration/RibbonConfiguration.java
+++ src/main/java/microservices/book/gateway/configuration/RibbonConfiguration.java
@@ -12,6 +12,22 @@ public class RibbonConfiguration {
 	
 	@Bean
 	public IPing ribbonPing(IClientConfig config) {
+		/**
+		 * Ping implementation if you want to do a "health check" kind of Ping. This
+		 * will be a "real" ping. As in a real http/s call is made to this url e.g.
+		 * http://ec2-75-101-231-85.compute-1.amazonaws.com:7101/cs/hostRunning
+		 * 
+		 * Some services/clients choose PingDiscovery - which is quick but is not a real
+		 * ping. i.e It just asks discovery (eureka) in-memory cache if the server is present
+		 * in its Roster PingUrl on the other hand, makes an actual call. This is more
+		 * expensive - but its the "standard" way most VIPs and other services perform
+		 * HealthChecks.
+		 * 
+		 * Choose your Ping based on your needs.
+		 * 
+		 * @author stonse
+		 * 
+		 */
 		return new PingUrl(false, "/health");
 	}
 	
