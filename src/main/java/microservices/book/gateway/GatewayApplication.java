5c85c7f2ddf61d98768179dd8ed2e5955151daef
sabaja <jacoposabatini76@gmail.com>
2019-03-27 22:59:45 +0100

    commit

--- src/main/java/microservices/book/gateway/GatewayApplication.java
+++ src/main/java/microservices/book/gateway/GatewayApplication.java
@@ -10,16 +10,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import microservices.book.gateway.configuration.RibbonConfiguration;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 //@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class GatewayApplication {
 
